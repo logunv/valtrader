43775eab3780f9694871eaa337621a2056154948
logunv <logunv@gmail.com>
2015-10-03 06:12:47 -0700

    Update NHNLData.java

--- src/com/valtrader/data/NHNLData.java
+++ src/com/valtrader/data/NHNLData.java
@@ -1,23 +1,16 @@
+/**
+ * NHNL - New High New Low object service.
+ */
+ 
 package com.valtrader.data;
 import java.sql.Date;
 import java.util.Calendar;
 import java.util.HashMap;
 
 public class NHNLData {
-//	static NHNL [] nhnldata = null;
 	static HashMap<String,NHNL[]> cache = new HashMap<String, NHNL[]>();
 	static MyDatabase db = MyDatabase.db;
 
-//	static public NHNL [] get(String period) {
-//		try {
-//		if(nhnldata == null) nhnldata = getData();
-//		} catch(Exception e) {
-//			System.err.println(e.getMessage());
-//		}
-//		
-//		return toPeriod(nhnldata, period);
-//		
-//	}
 	static public NHNL[] get(String period) {
 		try {
 			NHNL[] ret = cache.get(period);
@@ -70,68 +63,12 @@ public class NHNLData {
 		return nhnl;
 	}
 
-//	static NHNL [] getData() throws Exception {
-//		String sql = "select count(*) from nhnl";
-//		MyStatement st = db.execute(sql);
-//		if(!st.next()) return null;
-//		int nd = st.getInt(1);
-//		st.close();
-//		
-//		NHNL [] nhnl = new NHNL[nd];
-//		
-//		sql = "select * from nhnl order by day desc";
-//
-//		st = db.execute(sql);
-//		int row = 0;
-//		while (st.next()) {
-//			nhnl[row] = new NHNL(st.getDate("day"), st.getInt("newhigh"), st.getInt("newlow"));
-//			row++;
-//		}
-//
-//		st.close();
-//
-//		return nhnl;
-//	}
-
-//	public static void refreshCache() {
-//		nhnldata = null;
-//	}
-//	static public NHNL [] toPeriod(NHNL [] daily, String period) {
-//		if (period.equalsIgnoreCase("d"))
-//			return daily;
-//
-//		int prevday = 999, row = 0;
-//		NHNL [] ret = new NHNL[(int)(daily.length/5)+15];
-//		NHNL thisWeek = null;
-//		Date d = null;
-//		int nh = 0, nl = 0;
-//		for(int i = 0; i < daily.length - 1; i++) {
-//			NHNL n = daily[i];
-//			int curday = getWeekday(n.date);
-//			if(curday > prevday) {
-//				ret[row] = new NHNL(d, nh, nl);
-//				row++;
-//				d = n.date;
-//				nh=0;
-//				nl=0;
-//			}
-//			nh += n.nh;
-//			nl += n.nl;
-//			d = n.date;
-//			prevday = curday;
-//			
-//		}
-//		return ret;
-//
-//	}
-
 	static int getWeekday(Date d) {
 		// Create a calendar with year and day of year.
 		Calendar cal = Calendar.getInstance();
 		cal.setTimeInMillis(d.getTime());
 		return cal.get(Calendar.DAY_OF_WEEK);
 	}
-
 
 }
 
